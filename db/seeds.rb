12104869c3f04c0249f826dd2c11a5e98da53e72
Virginia Garcia <virginia.a.garmar@gmail.com>
2020-02-19 21:28:14 +0100

    restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  { name: 'Sushi Seki', address: 'Chelsea', category: 'japanese' },
+  { name: 'Buddakan', address: 'Meatpacking', category: 'chinese' },
+  { name: 'Via Carota', address: 'West Village', category: 'italian' },
+  { name: 'Daniel', address: 'Upper East Side', category: 'french' },
+  { name: 'Petit Abeille', address: 'East Village', category: 'belgian' }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
+Review.create(content: "Great mussels", rating: 3, restaurant_id: 5)
